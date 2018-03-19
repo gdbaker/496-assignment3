0b184867d51a323be705d670a05c5289c79fceae
gdbaker <gdbaker@ualberta.ca>
2018-03-18 20:12:41 -0600

    added stuff

--- Go3/gtp_connection_go3.py
+++ Go3/gtp_connection_go3.py
@@ -37,7 +37,7 @@ class GtpConnectionGo3(gtp_connection.GtpConnection):
 		"""
 		Return list of policy moves for the current_player of the board
 		"""
-		policy_moves, type_of_move = GoBoardUtil.generate_all_policy_moves(self.board,
+		policy_moves, type_of_move = self.generate_all_policy_moves(self.board,
 														self.go_engine.use_pattern,
 														self.go_engine.check_selfatari)
 		if len(policy_moves) == 0:
@@ -47,6 +47,19 @@ class GtpConnectionGo3(gtp_connection.GtpConnection):
 			self.respond(response)
 
 
+	def generate_all_policy_moves(self, board,pattern,check_selfatari):
+		"""
+			generate a list of policy moves on board for board.current_player.
+			Use in UI only. For playing, use generate_move_with_filter
+			which is more efficient
+		"""
+		if pattern:
+			pattern_moves = []
+			pattern_moves = GoBoardUtil.generate_pattern_moves(board)
+			pattern_moves = GoBoardUtil.filter_moves(board, pattern_moves, check_selfatari)
+			if len(pattern_moves) > 0:
+				return pattern_moves, "Pattern"
+		return GoBoardUtil.generate_random_moves(board,True), "Random"
 
 
 
