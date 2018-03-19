5fef27dd25f5058fdf3117a15c561a3fc579cfa9
gdbaker <gdbaker@ualberta.ca>
2018-03-18 22:21:49 -0600

    passes tests

--- Go3/gtp_connection_go3.py
+++ Go3/gtp_connection_go3.py
@@ -12,6 +12,7 @@ import sys
 import os
 from board_util import GoBoardUtil, BLACK, WHITE, EMPTY, BORDER, FLOODFILL
 import gtp_connection
+import numpy as np
 
 class GtpConnectionGo3(gtp_connection.GtpConnection):
 
@@ -73,13 +74,14 @@ class GtpConnectionGo3(gtp_connection.GtpConnection):
 				if len(defense_points) > 0:
 					return defense_points, "AtariDefense"
 
-
-
 		policy_moves, type_of_move = GoBoardUtil.generate_all_policy_moves(self.board, self.go_engine.use_pattern, self.go_engine.check_selfatari)
 		return policy_moves, type_of_move
 
 
 	def try_to_defend(self):
+		"""
+			tries possible ways to defend
+		"""
 		neighbours = self.board._neighbors(self.board.last_move)
 		plays = []
 		for point in neighbours:
@@ -89,11 +91,15 @@ class GtpConnectionGo3(gtp_connection.GtpConnection):
 				if liberty_point is not None:
 					# point is block of current player and is in atari
 					plays.extend(self.try_runaway(point))
+					plays.extend(self.try_capture(point))
 		return plays
 
 
 
 	def try_runaway(self, runner):
+		"""
+			finds possible defense by running away
+		"""
 		works = []
 		neighbours = self.board._neighbors(runner)
 		for point in neighbours:
@@ -106,7 +112,38 @@ class GtpConnectionGo3(gtp_connection.GtpConnection):
 		return works
 
 	def try_capture(self, capPoint):
+		"""
+			finds possible defense by capturing opponent
+		"""
 		works = []
+		opponentBlocks = self.modified_flood_fill(capPoint)
 
+		for point in opponentBlocks:
+			liberty = self.board._single_liberty(point, GoBoardUtil.opponent(self.board.current_player))
 
+			if liberty is not None and self.board.check_legal(liberty, self.board.current_player):
+				works.append(liberty)
+
+		return works
+
+	def modified_flood_fill(self, capPoint):
+		"""
+			modified floodfill from util folder to 
+			return the adjacent opponent points
+		"""
+		fboard = np.array(self.board.board, copy=True)
+		pointstack = [capPoint]
+		fboard[capPoint] = FLOODFILL
+		opponentBlocks = []
+		while pointstack:
+			current_point = pointstack.pop()
+			neighbors = self.board._neighbors(current_point)
+			for n in neighbors :
+				if fboard[n] == self.board.current_player:
+					fboard[n] = FLOODFILL
+					pointstack.append(n)
+				elif fboard[n] ==  GoBoardUtil.opponent(self.board.current_player):
+					fboard[n] = BORDER
+					opponentBlocks.append(n)
+		return opponentBlocks
 
